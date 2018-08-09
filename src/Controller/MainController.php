bb780d985f82b911e5b6e94437da3fbcba930a31
Rollo <juriens.rodrigue@gmail.com>
2018-08-09 18:00:08 +0200

    plannig view management(twig template)

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -4,6 +4,8 @@ namespace App\Controller;
 
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use App\Repository\CompanyRepository;
 
 class MainController extends Controller
 {
@@ -18,12 +20,24 @@ class MainController extends Controller
         }
         
         /**
-         * @Route("/home", name="home")
+         * @Route("/home", name="home", methods="GET|POST")
          */
-        public function home()
+        public function home(Request $request, CompanyRepository $companyRepo)
         {
+            $companyId = $request->request->all();
+            $company = $companyRepo->findOneById($companyId);
+
+            if ($companyId !== []) {
+                return $this->render('main/home.html.twig', [
+                    'controller_name' => 'MainController',
+                    'company' => $company,
+                    'page_title' => "Bienvenus ". $this->getUser()->getUsername(),
+            ]);           
+            }
+
             return $this->render('main/home.html.twig', [
                 'controller_name' => 'MainController',
+                'company' => $this->getUser()->getCompanies()[0],
                 'page_title' => "Bienvenus ". $this->getUser()->getUsername(),
         ]);
     }
