4c50efac1489f2441d831ffc51018a03e9e43720
RolloDMF <juriens.rodrigue@gmail.com>
2018-11-13 10:18:33 +0100

    design ok

---
+++ public/js/formManip.js
@@ -0,0 +1,6 @@
+var formManip = {
+    init: function(){
+        $('#employee_company').addClass('custom-select col-2');
+    },
+}
+$(formManip.init);
--- src/Form/EmployeeType.php
+++ src/Form/EmployeeType.php
@@ -8,19 +8,24 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\ColorType;
 use PhpParser\Node\Stmt\Label;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use App\Entity\Company;
 
 class EmployeeType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('firstName')
-            ->add('lastName')
-            ->add('email')
+            ->add('firstName', TextType::class, array('label' => 'Prénom '))
+            ->add('lastName', TextType::class, array('label' => 'Nom '))
+            ->add('email', EmailType::class, array('label' => 'E-mail '))
             ->add('color', ColorType::class, array('label' => 'couleur associée'))
-            ->add('numberOfHours')
-            ->add('remainingLeave')
-            ->add('company')
+            ->add('numberOfHours', IntegerType::class, array('label' => 'Heures Hebdomadaire '))
+            ->add('remainingLeave', IntegerType::class, array('label' => 'Jours de congés '))
+            ->add('company');
         ;
     }
 
